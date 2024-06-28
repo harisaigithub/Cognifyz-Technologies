7b90f95b559d461e7fcb4121c69789a74d4e0394
HARI SAI PARASA <harisaiparasa@gmail.com>
2024-06-28 11:29:35 +0530

    updated outputs images

--- Data Science/LEVEL-1/TASK_2/task2.py
+++ Data Science/LEVEL-1/TASK_2/task2.py
@@ -1,9 +1,6 @@
-#level-1 Task-2
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-# Read the dataset from CSV file
 
 df = pd.read_csv(r'C:\Users\haris\OneDrive - Vasireddy Venkatadri Institute of Technology\Desktop\internship certificates\Cognifyz Technologies\Data Science\Dataset.csv')
 
@@ -18,17 +15,37 @@ country_counts = df['Country Code'].value_counts()
 city_counts = df['City'].value_counts()
 cuisine_counts = df['Cuisines'].value_counts()
 
-# Visualize distribution of categorical variables
+
+# Visualize distribution of 'Country Code'
 plt.figure(figsize=(10, 6))
 sns.barplot(x=country_counts.index, y=country_counts.values)
 plt.title('Distribution of Country Codes')
 plt.xlabel('Country Code')
 plt.ylabel('Count')
+plt.xticks(rotation=90)  # Rotate x-axis labels if there are many categories
+plt.show()
+
+# Visualize distribution of 'City'
+plt.figure(figsize=(10, 6))
+sns.barplot(x=city_counts.index, y=city_counts.values)
+plt.title('Distribution of Cities')
+plt.xlabel('City')
+plt.ylabel('Count')
+plt.xticks(rotation=90)  # Rotate x-axis labels if there are many categories
+plt.show()
+
+# Visualize distribution of 'Cuisines'
+plt.figure(figsize=(10, 6))
+sns.barplot(x=cuisine_counts.index, y=cuisine_counts.values)
+plt.title('Distribution of Cuisines')
+plt.xlabel('Cuisines')
+plt.ylabel('Count')
+plt.xticks(rotation=90)  # Rotate x-axis labels if there are many categories
 plt.show()
 
 
-# Repeat similar visualization for 'City' and 'Cuisines'
 top_cuisines = df['Cuisines'].value_counts().head(10)
+
 print("Top cuisines with the highest number of restaurants:")
 print(top_cuisines)
 
